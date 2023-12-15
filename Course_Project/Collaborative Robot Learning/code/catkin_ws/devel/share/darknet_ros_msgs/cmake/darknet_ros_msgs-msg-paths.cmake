7ff514bf8459e573a977e74aee89a8fc70643d27
Furylong <95402284+Furylong@users.noreply.github.com>
2023-12-15 21:46:15 +0800

    Add files via upload

---
+++ Course_Project/Collaborative Robot Learning/code/catkin_ws/devel/share/darknet_ros_msgs/cmake/darknet_ros_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(darknet_ros_msgs_MSG_INCLUDE_DIRS "/home/merryzyk/catkin_ws/src/roscar_gazebo_yolov4/darknet_ros/darknet_ros_msgs/msg;/home/merryzyk/catkin_ws/devel/share/darknet_ros_msgs/msg")
+set(darknet_ros_msgs_MSG_DEPENDENCIES actionlib_msgs;geometry_msgs;sensor_msgs;std_msgs)
---
+++ Course_Project/Collaborative Robot Learning/code/catkin_ws/devel/share/gennodejs/ros/darknet_ros_msgs/msg/_index.js
@@ -0,0 +1,26 @@
+
+"use strict";
+
+let BoundingBoxes = require('./BoundingBoxes.js');
+let ObjectCount = require('./ObjectCount.js');
+let BoundingBox = require('./BoundingBox.js');
+let CheckForObjectsResult = require('./CheckForObjectsResult.js');
+let CheckForObjectsActionFeedback = require('./CheckForObjectsActionFeedback.js');
+let CheckForObjectsFeedback = require('./CheckForObjectsFeedback.js');
+let CheckForObjectsActionGoal = require('./CheckForObjectsActionGoal.js');
+let CheckForObjectsActionResult = require('./CheckForObjectsActionResult.js');
+let CheckForObjectsGoal = require('./CheckForObjectsGoal.js');
+let CheckForObjectsAction = require('./CheckForObjectsAction.js');
+
+module.exports = {
+  BoundingBoxes: BoundingBoxes,
+  ObjectCount: ObjectCount,
+  BoundingBox: BoundingBox,
+  CheckForObjectsResult: CheckForObjectsResult,
+  CheckForObjectsActionFeedback: CheckForObjectsActionFeedback,
+  CheckForObjectsFeedback: CheckForObjectsFeedback,
+  CheckForObjectsActionGoal: CheckForObjectsActionGoal,
+  CheckForObjectsActionResult: CheckForObjectsActionResult,
+  CheckForObjectsGoal: CheckForObjectsGoal,
+  CheckForObjectsAction: CheckForObjectsAction,
+};
